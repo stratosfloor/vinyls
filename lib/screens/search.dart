b51fe6d1404e515ca2389ef083215a971c4cb46c
Emil Noren <stratosfloor@hotmail.com>
2023-09-06 13:14:42 +0200

    Implemented searchbar widget

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:wanted_vinyls/screens/widgets/search.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Search'),
+    return const Column(
+      children: [
+        Search(),
+        Expanded(
+          child: Center(
+            child: Text('CENTER'),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/screens/widgets/search.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatefulWidget {
+  const Search({super.key});
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  final _searchController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 14,
+        right: 14,
+        bottom: 14,
+        top: 14,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              autofocus: false,
+              controller: _searchController,
+              textCapitalization: TextCapitalization.none,
+              autocorrect: false,
+              enableSuggestions: true,
+              decoration: InputDecoration(
+                hintText: 'Search for album',
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    style: BorderStyle.solid,
+                    color: Theme.of(context).colorScheme.onBackground,
+                    width: 1,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          IconButton(
+            color: Theme.of(context).colorScheme.onBackground,
+            icon: const Icon(
+              Icons.search,
+            ),
+            onPressed: () {
+              print(_searchController.text);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
