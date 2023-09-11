d92a18368e3036d66d80ab3a8099cb3cd3dfc349
Emil Noren <stratosfloor@hotmail.com>
2023-09-11 11:03:15 +0200

    Implemented sorting by artist, title, year, asc and desc

--- lib/screens/collection.dart
+++ lib/screens/collection.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wanted_vinyls/providers/albums.dart';
 import 'package:wanted_vinyls/widgets/album_list.dart';
 
+import '../models/album.dart';
+
 class CollectionScreen extends ConsumerStatefulWidget {
   const CollectionScreen({super.key});
 
@@ -12,11 +14,47 @@ class CollectionScreen extends ConsumerStatefulWidget {
 
 class _CollectionScreenState extends ConsumerState<CollectionScreen> {
   late Future<void> _listFuture;
+  bool _artistAscending = true;
+  bool _titleAscending = true;
+  bool _yearAscending = true;
 
   @override
   void initState() {
     super.initState();
     _listFuture = ref.read(albumsProvider.notifier).loadMyCollection();
+  }
+
+  void sortListByArtist(List<Album> list) {
+    setState(() {
+      if (_artistAscending) {
+        list.sort((a, b) => a.artist.compareTo(b.artist));
+      } else {
+        list.sort((b, a) => a.artist.compareTo(b.artist));
+      }
+      _artistAscending = !_artistAscending;
+    });
+  }
+
+  void sortListByTitle(List<Album> list) {
+    setState(() {
+      if (_titleAscending) {
+        list.sort((a, b) => a.title.compareTo(b.title));
+      } else {
+        list.sort((b, a) => a.title.compareTo(b.title));
+      }
+      _titleAscending = !_titleAscending;
+    });
+  }
+
+  void sortListByYear(List<Album> list) {
+    setState(() {
+      if (_yearAscending) {
+        list.sort((a, b) => a.year.compareTo(b.year));
+      } else {
+        list.sort((b, a) => a.year.compareTo(b.year));
+      }
+      _yearAscending = !_yearAscending;
+    });
   }
 
   @override
@@ -31,34 +69,40 @@ class _CollectionScreenState extends ConsumerState<CollectionScreen> {
         trackVisibility: false,
         child: Column(
           children: [
-            const Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Row(
                     mainAxisSize: MainAxisSize.min,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text('Artist'),
+                      const Text('Artist'),
                       IconButton(
                         iconSize: 20,
-                        onPressed: null,
-                        icon: Icon(Icons.arrow_circle_down),
+                        onPressed: () {
+                          sortListByArtist(list);
+                        },
+                        icon: const Icon(Icons.arrow_circle_down),
                       ),
                     ]),
                 Row(mainAxisSize: MainAxisSize.min, children: [
-                  Text('Title'),
+                  const Text('Title'),
                   IconButton(
                     iconSize: 20,
-                    onPressed: null,
-                    icon: Icon(Icons.arrow_circle_down),
+                    onPressed: () {
+                      sortListByTitle(list);
+                    },
+                    icon: const Icon(Icons.arrow_circle_down),
                   ),
                 ]),
                 Row(mainAxisSize: MainAxisSize.min, children: [
-                  Text('Year'),
+                  const Text('Year'),
                   IconButton(
                     iconSize: 20,
-                    onPressed: null,
-                    icon: Icon(Icons.arrow_circle_down),
+                    onPressed: () {
+                      sortListByYear(list);
+                    },
+                    icon: const Icon(Icons.arrow_circle_down),
                   ),
                 ]),
               ],
